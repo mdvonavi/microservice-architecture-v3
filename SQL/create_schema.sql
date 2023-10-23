b23126ac44bef48d5a3659f453baa9d345acfc6a
mdvonavi <mdvonavi@gmail.com>
2023-10-23 19:45:53 +0300

    add create schema sql

---
+++ SQL/create_schema.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE microservice;
+
+CREATE SCHEMA dev;
+
+CREATE TABLE dev.hard_skills
+(
+    id SERIAL,
+    name varchar(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dev.cities
+(
+    id SERIAL,
+    name varchar(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dev.images
+(
+    id varchar(10),
+    link varchar(2048) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dev.users
+(
+    id bigserial,
+	first_name varchar(30),
+	middle_name varchar(30),
+	last_name varchar(30),
+	sex integer,
+	birth_date date,
+	city int references dev.cities(id),
+	avatar varchar(10) references dev.images(id),
+	info varchar(256),
+	nickname varchar(20) NOT NULL,
+	email varchar(30),
+    phone varchar(11),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dev.users_hard_skills
+(
+	hard_skill int references dev.hard_skills(id) NOT NULL,
+	user_id bigserial references dev.users(id) NOT NULL
+);
+
+CREATE TABLE dev.subscriptions
+(
+	user_id bigserial references dev.users(id) NOT NULL,
+	subs_id bigserial references dev.users(id) NOT NULL
+);
