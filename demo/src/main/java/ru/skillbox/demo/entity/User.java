1c51680b8e177b3b54e22743c6653ce09f241d66
mdvonavi <mdvonavi@gmail.com>
2023-11-10 12:00:15 +0300

    добавлены поля для user, soft-delete записей этой таблицы

--- demo/src/main/java/ru/skillbox/demo/entity/User.java
+++ demo/src/main/java/ru/skillbox/demo/entity/User.java
@@ -1,11 +1,15 @@
 package ru.skillbox.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,13 +29,41 @@ public class User {
 
     private String firstName;
     private String lastName;
+    private String middleName;
+    private Boolean sex;
+    private Date birthDate;
+    private Integer city;
+    private String avatar;
+    private String info;
+    private String nickname;
+    private String email;
+    private String phone;
+    private boolean deleted = Boolean.FALSE;
 
-    public User() {
-    }
-
-    public User(String firstName, String lastName) {
+    public User(
+            String firstName,
+            String lastName,
+            String middleName,
+            Boolean sex,
+            Date birthDate,
+            Integer city,
+            String avatar,
+            String info,
+            String nickname,
+            String email,
+            String phone
+    ) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.middleName = middleName;
+        this.sex = sex;
+        this.birthDate = birthDate;
+        this.city = city;
+        this.avatar = avatar;
+        this.info = info;
+        this.nickname = nickname;
+        this.email = email;
+        this.phone = phone;
     }
 
     @Override
@@ -40,6 +72,7 @@ public class User {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
                 '}';
     }
 }
